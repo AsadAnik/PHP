923c727ee87d97aec376c975dfb678a4744db5c6
Asad Anik <asadanik@Asads-MacBook-Pro.local>
2020-12-26 15:49:28 +0600

    constant magic constant

--- Core@Syntex/constant_magic_constant.php
+++ Core@Syntex/constant_magic_constant.php
@@ -7,9 +7,46 @@ echo "USER_NAME: ".USER_NAME;
 echo "\n";
 
 //Declaring with case-sensativity..
-define("username", "wembledon", true);
-echo "username: ".Username;
+define("username", "wembledon", false);
+echo "username: ".username."\n";
+
+//Another way of Declaring Constant Variable..
+const yourName = "Tomas Adreson!";
+echo("Your Name : ".yourName."\n");
 
 
 ///==== The Magic Constants ====///
-//See the Exact File location..
+// __LINE__ ...
+echo __LINE__ ."\n";
+
+// __FILE__ ...
+echo __FILE__ ."\n";
+
+// __DIR__ ...
+echo __DIR__ ."\n";
+
+// __FUNCTION__ ...
+echo __FUNCTION__ ."\n";
+
+// __CLASS__ ...
+echo __CLASS__ ."\n";
+
+// __TRAIT__ ...
+echo __TRAIT__ ."\n";
+
+// __METHOD__ ...
+echo __METHOD__ ."\n";
+
+// __NAMESPACE__ ...
+echo __NAMESPACE__ ."\n";
+
+///==== The Predefined Constants ====///
+// PHP_VERSION ...
+echo PHP_VERSION ."\n";
+
+// PHP_EOL ...(new line between)
+echo "Hello All,";
+echo PHP_EOL ."\n";
+echo "How are you!. \n";
+
+
